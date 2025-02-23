4f0db3b11f70965b62443fb84536f04ad4efe246
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-02-23 11:29:45 +0330

    Create User Dashboard - Profile Layout

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -1,5 +1,20 @@
+"use client";
+import SvgLoaderComponent from "@/common/SvgLoaderComponent";
+import { useGetUser } from "@/hooks/useAuth";
+import toLocalDateShort from "@/utils/toLocalDateShort";
+
 function Profile() {
-  return <div>صفحه پروفایل کاربر</div>;
+  const { user, isLoading } = useGetUser();
+  if (isLoading) return <SvgLoaderComponent />;
+  return (
+    <div>
+      <h1>سلام! {user.name} خوش اومدی!</h1>
+      <p>
+        <span>تاریخ پیوستن:</span>
+        <span>{toLocalDateShort(user.createdAt)}</span>
+      </p>
+    </div>
+  );
 }
 
 export default Profile;
