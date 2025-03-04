e3a9c73fa4bd5e78e37cc5b92e0b3367d5fee27a
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-03-05 00:03:56 +0330

    create dashboard page in profile route

--- src/app/(profile)/profile/page.jsx
+++ src/app/(profile)/profile/page.jsx
@@ -1,18 +1,37 @@
 "use client";
-import SvgLoaderComponent from "@/common/SvgLoaderComponent";
 import { useGetUser } from "@/hooks/useAuth";
 import toLocalDateShort from "@/utils/toLocalDateShort";
+import PaymentTable from "./payments/PaymentTable";
+import Link from "next/link";
+import Loading from "@/common/Loading";
 
 function Profile() {
-  const { user, isLoading } = useGetUser();
-  if (isLoading) return <SvgLoaderComponent />;
+  const { user, payments, isLoading } = useGetUser();
+  if (isLoading) return <Loading />;
   return (
     <div>
-      <h1>سلام! {user.name} خوش اومدی!</h1>
+      <h1 className="mb-2">
+        سلام!{" "}
+        {<span className="font-semibold text-secondary-800">{user.name}</span>}{" "}
+        خوش اومدی!
+      </h1>
       <p>
         <span>تاریخ پیوستن:</span>
         <span>{toLocalDateShort(user.createdAt)}</span>
       </p>
+      <div className="border rounded-xl p-4 mt-8">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">آخرین سفارشات کاربر</h2>
+          <Link href="/profile/payments" className="text-primary-900">
+            مشاهده همه سفارشات
+          </Link>
+        </div>
+        <PaymentTable
+          payments={payments
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+            .slice(0, 3)}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/app/(profile)/profile/payments/PaymentTable.jsx
@@ -0,0 +1,73 @@
+import { userPaymentTHeads } from "@/constants/tableHeads";
+import {
+  toPersianNumbers,
+  toPersianNumbersWithComma,
+} from "@/utils/numberFormatter";
+import toLocalDateShort from "@/utils/toLocalDateShort";
+
+function PaymentTable({ payments }) {
+  return (
+    <div className="shadow-sm overflow-auto my-8">
+      <table className="border-collapse table-auto w-full min-w[800px] text-sm">
+        <thead>
+          <tr>
+            {userPaymentTHeads.map((item) => {
+              return (
+                <th className="whitespace-nowrap table__th" key={item.id}>
+                  {item.label}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {payments.map((payment, index) => {
+            return (
+              <tr key={payment._id}>
+                <td className="table__td">{toPersianNumbers(index + 1)}</td>
+                <td className="table__td">
+                  {toPersianNumbers(payment.invoiceNumber)}
+                </td>
+                <td
+                  className="table__td max-w-[280px] whitespace-nowrap truncate"
+                  title={payment.description}
+                >
+                  {payment.description}
+                </td>
+                <td className="table__td">
+                  <div className="flex flex-col gap-y-2 items-start">
+                    {payment.cart.productDetail.map((product) => {
+                      return (
+                        <span
+                          key={product._id}
+                          className="badge badge--secondary"
+                        >
+                          {product.title}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </td>
+                <td className="table__td">
+                  {toPersianNumbersWithComma(payment.amount)}
+                </td>
+                <td className="table__td">
+                  {toLocalDateShort(payment.createdAt)}
+                </td>
+                <td className="table__td">
+                  {payment.status === "COMPLETED" ? (
+                    <span className="badge badge--success">موفق</span>
+                  ) : (
+                    <span className="badge badge--error">ناموفق</span>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default PaymentTable;
