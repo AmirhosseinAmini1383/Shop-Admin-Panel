56226966a8b12935f58af841b8de934ae9cf2811
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2025-03-08 00:49:18 +0330

    add new product form

--- src/app/(user)/layout.jsx
+++ src/app/(user)/layout.jsx
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`${vazirFont.variable} font-sans`}>
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}
+      >
         <ReactQueryProvider>
           <Toaster />
           <Header />
